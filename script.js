77b2259b3158b2616217dc4fe2e9d38f7a27a90e
Ollivierthomas <ollivierthomas4@gmail.com>
2024-03-10 14:08:43 +0100

    modified:   index.html 	modified:   script.js 	modified:   styles.css

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open')
+};
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.logo',{delay:200, origin: 'left'});
+sr.reveal('.navbar',{delay:400, origin: 'top'});
+sr.reveal('.menu-btn',{delay:520, origin: 'right'});
+
+sr.reveal('.home-text span',{delay:600, origin: 'top'});
+sr.reveal('.home-text h1',{delay:680, origin: 'left'});
+sr.reveal('.home-text p',{delay:750, origin: 'right'});
+sr.reveal('.main-btn',{delay:860, origin: 'left'});
+
+sr.reveal('.share',{delay:950, origin: 'bottom'});
+
+sr.reveal('.home-img',{delay:1000, origin: 'right'});
